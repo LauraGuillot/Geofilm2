b787e1cb95099f94e89fe48d9bd1c2673169bb80
LauraGuillot <lguillot@eleves.ec-nantes.fr>
2017-03-19 17:45:19 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	build/web/WEB-INF/classes/Servlets/UploadServlet.class
    #	src/java/Servlets/UploadServlet.java
    #	web/Scripts/upload.js

--- src/java/Servlets/UploadFileServlet.java
+++ src/java/Servlets/UploadFileServlet.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import org.springframework.web.servlet.ModelAndView;
 
 @WebServlet(name = "UploadFileServlet", urlPatterns = {"/UploadFileServlet"})
 public class UploadFileServlet extends HttpServlet {
--- web/Scripts/strings.js
+++ web/Scripts/strings.js
@@ -117,6 +117,7 @@ var error_upload_file_fr = "Le format du fichier n'est pas pris en charge.";
 var true_fr = "vrai";
 var location_ok_fr = "Localisation ok";
 var error_file_none_fr = "Veuillez choisir un fichier.";
+var error_multimedia_already_entered_fr = "Le multimédia n'a pas été uploadé.";
 
 var upload_confirmed_done_fr = "Votre multimédia a bien été uploadé.";
 var valid_upload_fr = "FERMER";
